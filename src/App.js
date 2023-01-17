b8f7441e6bdcf8ea33b8ff0be8475082dbf94c31
elenaB1864 <alina.balanescu1864@gmail.com>
2023-01-17 23:01:31 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a href="https://hardcore-swartz-221fac.netlify.app/">
-          Elena Alina Balanescu
-        </a>{" "}
-        and is {""}
-        <a href="https://github.com/elenaB1864">open-sourced</a>.
-      </footer>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://hardcore-swartz-221fac.netlify.app/">
+            Elena Alina Balanescu
+          </a>{" "}
+          and is {""}
+          <a href="https://github.com/elenaB1864">open-sourced</a>.
+        </footer>
+      </div>
     </div>
   );
 }
