43c506f99deadad57cac08a80012ed5095936a43
elenaB1864 <alina.balanescu1864@gmail.com>
2023-01-17 21:42:55 +0200

    Added GitHub repository url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by{" "}
+        <a href="https://hardcore-swartz-221fac.netlify.app/">
+          Elena Alina Balanescu
+        </a>{" "}
+        and is {""}
+        <a href="https://github.com/elenaB1864">open-sourced</a>.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
