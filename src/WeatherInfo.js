03cb01738489d44259d3cc0be075c0e94ee72204
elenaB1864 <alina.balanescu1864@gmail.com>
2023-01-22 16:52:33 +0200

    Created new component to convert temperature from celsius to fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
@@ -12,25 +13,18 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix ">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} />
-            </div>
+        <div className="col-lg-6 col-md-6 col-sm-8">
+          <div className="temperature-container d-flex justify-content-start ">
+            <WeatherIcon code={props.data.icon} />
 
-            <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
-            </div>
+            <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
 
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.data.humidity} </li>
-            <li>Wind: {props.data.wind}</li>
+            <li>Humidity:{props.data.humidity} %</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
